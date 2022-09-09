e705fe22edc56bf8cf56d8dd65f29f5334ea6128
Oluborode Victor <oluborodevictor110@gmail.com>
2022-09-08 20:20:30 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print the string in the function
  *
- * Return: Always 0 (Success)
+ * Description: using the main function
+ * the program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("With proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
