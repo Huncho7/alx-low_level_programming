458903957618bc7d56c36cd04158f2a627e6ab2d
Oluborode Victor <oluborodevictor110@gmail.com>
2022-09-08 20:24:13 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
+5. #include <stdio.h>
 
 /**
- * main - print the string in the function
+ * main - Entry point
  *
- * Description: using the main function
- * the program prints "Programming is like building a multilingual puzzle
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("With proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
