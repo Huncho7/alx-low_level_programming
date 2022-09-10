7e42cf3f76c879b4f5a66b3681638652c6406bed
Oluborode Victor <oluborodevictor110@gmail.com>
2022-09-09 21:43:02 -0700

    commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@
  * this program prints "different combinations of two digits"
  * Return: 0
  */
-int main()
+int main(void)
 {
 	int digit1, digit2;
 
-	for(digit1 = 0; digit1 < 9; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for(digit2 = digit1 + 1; digit2 < 10; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
