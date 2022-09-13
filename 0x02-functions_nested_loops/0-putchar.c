936ee2bad3c555ebc3b4c8d5d9d2306d78452e3a
Oluborode Victor <oluborodevictor110@gmail.com>
2022-09-13 09:32:19 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints _putchar followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar("_putchar\n");
+
+	Return(0);
+}
